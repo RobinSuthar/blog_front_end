cc9fad5eb6cfbafeb5c769c6be5692996bc89661
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-03-20 00:58:45 -0600

    Added Carsouel

--- front-end/src/democomponents/FlipWordsDemo.tsx
+++ front-end/src/democomponents/FlipWordsDemo.tsx
@@ -1,9 +1,10 @@
-import { FlipWords } from "../components/ui/flip-words";
+// import { FlipWords } from "../components/ui/flip-words";
 import { Button } from "../components/ui/button";
 import { useNavigate } from "react-router-dom";
+import { CarouselDemo } from "./CarouselDemo";
 
 export function FlipWordsDemo() {
-  const words = ["Anonumus", "Post", "React"];
+  // const words = ["Anonumus", "Post", "React"];
   const navigate = useNavigate();
 
   function handleClick() {
@@ -12,10 +13,10 @@ export function FlipWordsDemo() {
 
   return (
     <div className="h-[40rem] flex flex-col justify-center items-center px-4">
-      <div className="text-7xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        Welcome! SAIT Student!
-        <div>
-          <FlipWords words={words} /> <br />
+      <div className="  mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+        <div className="flex flex-row">
+          {/* <FlipWords words={words} /> <br /> */}
+          <CarouselDemo />
         </div>
       </div>
       <div className="mt-16">
