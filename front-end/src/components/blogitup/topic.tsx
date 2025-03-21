4f6775ff6c425531bd5dba5d11eb27cb7faca5af
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-03-20 20:39:42 -0600

    Naviation Bar

---
+++ front-end/src/components/blogitup/topic.tsx
@@ -0,0 +1,25 @@
+const Topic = () => {
+  return (
+    <div>
+      <div className="flex justify-evenly">
+        <h1>Student </h1>
+
+        <h1>Teacher</h1>
+
+        <h1>Software Development</h1>
+
+        <h1>Technology</h1>
+
+        <h1>AI</h1>
+
+        <h1>Cless</h1>
+
+        <h1>Election</h1>
+
+        <h1>Games</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Topic;
